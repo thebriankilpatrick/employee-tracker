117b5aa57b19c8f88f009aa9d4f114a9031a6b64
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-02-01 20:57:24 -0500

    Added seed.sql, initial inquirer functionality, and mysql connection

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "1722",
+    database: "employee_cms"
+});
+  
+connection.connect(err => {
+    if (err) throw err;
+    // initialPrompt()
+});
+
+function initialPrompt() {
+    inquirer.prompt([
+        {
+           type: "list",
+           name: "initQuestion",
+           message: "What would you like to do?",
+           choices: ["Add Employee", "Add Role", "Add Department",
+                "View Departments", "View Roles", "View Employees", "Update Employee Role"]
+        }
+    ]).then(answer => {
+        console.log(answer.initQuestion);
+    })
+}
