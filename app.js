d7f3489f2bb7c1378d7b1c066760b4bc8b78183c
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-02-03 20:31:55 -0500

    Added base functionality, and inquirer prompts

--- app.js
+++ app.js
@@ -26,5 +26,123 @@ function initialPrompt() {
         }
     ]).then(answer => {
         console.log(answer.initQuestion);
+        
+        switch(answer.initQuestion) {
+            case "Add Employee":
+                addEmployee();
+            break;
+
+            case "Add Role":
+                addRole();
+            break;
+
+            case "Add Department":
+                addDepartment();
+            break;
+
+            case "View Departments":
+                viewDepartments();
+            break;
+
+            case "View Roles":
+                viewRoles();
+            break;
+
+            case "View Employees":
+                viewEmployees();
+            break;
+
+            case "Update Employee Role":
+                updateEmployee();
+            break;
+        }
     })
 }
+
+// -------------------- Function for Adding Employee ---------------------
+function addEmployee() {
+    console.log("Adding Employee");
+    inquirer.prompt([
+        // Probably need to add info about role and manager
+        {
+            type: "input",
+            name: "firstName",
+            message: "What is the employee's first name?"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "What is the employee's last name?"
+        }
+    ]).then(answers => {
+        console.log(answers.firstName);
+        console.log(answers.lastName);
+    })
+}
+
+// ------------------------- Function for Adding Role -----------------------
+function addRole() {
+    console.log("Adding Role");
+
+    inquirer.prompt([
+        // Probably need to add info about department
+        {
+            type: "input",
+            name: "roleTitle",
+            message: "What is the role's title?"
+        },
+        {
+            type: "input",
+            name: "roleSalary",
+            message: "What is the role's base salary?"
+        }
+    ]).then(answers => {
+        console.log(answers.roleTitle);
+        console.log(answers.roleSalary);
+    })
+}
+
+// ------------------------ Function for adding Department ------------------
+function addDepartment() {
+    console.log("Adding Department");
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "departmentName",
+            message: "What is the department's name?"
+        }
+    ]).then(answer => {
+        console.log(answer.departmentName);
+    })
+}
+
+function viewDepartments() {
+    console.log("Viewing Departments");
+}
+
+function viewRoles() {
+    console.log("Viewing Roles");
+}
+
+function viewEmployees() {
+    console.log("Viewing Employees");
+}
+
+// --------------------------- Function for updating employee ------------
+function updateEmployee() {
+    console.log("Updating Employee Roles");
+
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "chooseEmployee",
+            message: "Which employee would you like to update?",
+            choices: []
+        }
+    ]).then(answers => {
+        console.log(answers.chooseEmployee);
+    })
+}
+
+// initialPrompt();
